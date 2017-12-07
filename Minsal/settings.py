fb312f117ecf68c4754a7d77629c6f319385be65
Fernando <fernandorivasarevalo27@gmail.com>
2017-12-07 02:59:07 -0600

    Agregando

--- Minsal/settings.py
+++ Minsal/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'Minsal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'minsal_2',
+        'NAME': 'minsal',
         'USER': 'igf_gpo6',
         'PASSWORD': 'django',
         'HOST': 'localhost',
